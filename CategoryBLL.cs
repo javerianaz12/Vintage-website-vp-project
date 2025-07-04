45b1aca88df27b4df4a431d2e311a5a0320d1be3
javerianaz12 <221007@students.au.edu.pk>
2025-07-04 12:08:54 +0500

    Add files via upload
    - Implemented 3-tier architecture (DAL, BLL, Entities, Interfaces)
    - Added SQL Server database with normalized tables: Users, Categories, Products
    - Developed full CRUD for Product and Category using stored procedures and ADO.NET
    - Integrated Blazor components with proper data binding and dependency injection
    - Designed responsive UI using Bootstrap
    - Created reusable components: Navbar, Footer
    - Connected navigation with Razor pages (ProductList, AddProduct)
    - Setup Google OAuth login (ASP.NET Core Identity)
    - Included DBHelper.cs for secure connection handling
    - Prepared for local deployment and future scalability

---
+++ CategoryBLL.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Vintage.Entities;
+using Vintage.Interfaces;
+
+namespace Vintage.BLL
+{
+    public class CategoryBLL
+    {
+        private readonly ICategoryDAL categoryDAL;
+
+        public CategoryBLL(ICategoryDAL categoryDAL)
+        {
+            this.categoryDAL = categoryDAL;
+        }
+
+        public List<Category> Getcategories()
+        {
+            return categoryDAL.Getcategories();
+        }
+
+        public Category GetcategoryById(int id)
+        {
+            return categoryDAL.GetcategoryById(id);
+        }
+
+        public void Addcategory(Category category)
+        {
+            categoryDAL.Addcategory(category);
+        }
+
+        public void Updatecategory(Category category)
+        {
+            categoryDAL.Updatecategory(category);
+        }
+
+        public void Deletecategory(int id)
+        {
+            categoryDAL.Deletecategory(id);
+        }
+    }
+}
---
+++ productBLL.cs
@@ -0,0 +1,40 @@
+﻿using Vintage.Entities;
+using System.Collections.Generic;
+using Vintage.Interfaces;
+namespace Vintage.BLL
+{
+    public class productBLL
+    {
+        private readonly IProductDAL productDAL;
+
+        public productBLL(IProductDAL ProductDAL)
+        {
+            this.productDAL = ProductDAL;
+        }
+
+        public List<Product> Getproduct()
+        {
+            return productDAL.Getproduct();
+        }
+
+        public void Addproduct(Product product)
+        {
+            productDAL.Addproduct(product);
+        }
+
+        public void Updateproduct(Product product)
+        {
+            productDAL.Updateproduct(product);
+        }
+
+        public void Deleteproduct(int id)
+        {
+            productDAL.Deleteproduct(id);
+        }
+
+        public void Getproductbyid(int id)
+        {
+            productDAL.Getproductbyid(id);
+        }
+    }
+}
